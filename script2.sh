df1d6115fc1aa759fda7a156a9c7a6e6b2f9d698
Banggottem <138918241+Banggottem@users.noreply.github.com>
2023-07-24 13:15:49 -0500

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # example of using arguments to a script
-echo "My first name is $1"
+echo "My first name is $1" # my first name is 
 echo "My surname is $2"
 echo "Total number of arguments is $#"
