b990170140b1f2d9c4c4ab26de7a7e487b453b65
Banggottem <138918241+Banggottem@users.noreply.github.com>
2023-07-24 13:36:06 -0500

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -2,16 +2,16 @@
 x=10 #NOT x = 10 no spaces
 X=20 #variables are case sensitive
 y= #NULL variable
-echo "x = $x"
-echo "X = $X"
-echo "y = $y"
+echo "x = $x" #x=
+echo "X = $X" #X=
+echo "y = $y" #y=
 script4.sh
 #!/bin/bash
 input =“./names.txt”
 while IFS=, read -r first last; # notice the comma after the equal sign
 do
-name="$first $last"
-username="${first:0:1}$last"
-useradd -m $username -s /bin/bash
-done < "$input"
-tail -8 /etc/passwd
+name="$first $last" #name=
+username="${first:0:1}$last"#username=
+useradd -m $username -s /bin/bash # useradd -m -s /bin/bash
+done < "$input" #finsh script 
+tail -8 /etc/passwd # tells last part of password  
